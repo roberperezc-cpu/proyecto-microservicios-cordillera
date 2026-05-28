c148d31a42d1efcbc58de3f2307626df40a948e7
roberperezc-cpu <rober.perezc@duocuc.cl>
2026-05-28 19:28:03 -0400

    Delete group-cordillera directory

--- group-cordillera/src/main/java/com/group_cordillera/model/CanalVenta.java
+++
@@ -1,6 +0,0 @@
-package com.group_cordillera.model;
-
-public enum CanalVenta {
-    POS,        // Representa los Sistemas de punto de venta físicos
-    ECOMMERCE   // Representa las Plataformas de comercio electrónico
-}
